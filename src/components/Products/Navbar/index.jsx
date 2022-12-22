c5efee918f99f983b3a8705b51f566ef9e9ed469
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-12-22 13:47:37 +0100

    prod

--- src/components/Products/Navbar/index.jsx
+++ src/components/Products/Navbar/index.jsx
@@ -8,7 +8,7 @@ import {
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 import { Link, useLocation } from "react-router-dom";
-import logo from "./img/logo.jpg";
+// import logo from "./logo.jpg";
 
 import "./style.css";
 
@@ -26,7 +26,7 @@ const NavBar = ({ basketItems, totalCost }) => {
 							variant='h6'
 							className='custom-title'
 							color='inherit'>
-							<img src={logo} alt='logo' height='25px' className='logo' />
+							{/* <img src={logo} alt='logo' height='25px' className='logo' /> */}
 						</Typography>
 						{location.pathname === "/basket" ? (
 							<div className='basket-wrapper'>
